b086bc0516c0bdd43608b7768047f1398b1855d0
Moussa <mm6824963@gmail.com>
2021-11-22 10:19:10 +0100

    Initial commit

---
+++ src/fr/declaration/variable/DeclarationAPP.java
@@ -0,0 +1,23 @@
+package fr.declaration.variable;
+
+public class DeclarationAPP {
+
+    public static void main(String[] args)
+    {
+        byte premiereVariable= 1;
+        short second= 2;
+        int third= 200;
+        long fourth = 1000000;
+        float fiveth= 2.2f;
+        double sixth= 3.3;
+        boolean seventh=true;
+        String eight="";
+
+        System.out.println("La variable byte contient "+premiereVariable);
+
+        String randomString = "Voici le résultat d’un calcul : \n 1+5=6";
+        System.out.println(randomString);
+
+
+    }
+}
---
+++ src/fr/declaration/variable/Main.java
@@ -0,0 +1,9 @@
+package fr.declaration.variable;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+}
